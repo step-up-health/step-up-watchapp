eb3e5df9b2a4d53946f3a284eb8cd312833b9092
jneubrand <johannes_n@icloud.com>
2016-01-31 11:46:36 -0800

    Initial Public Commit

---
+++ src/competitivehealth.c
@@ -0,0 +1,320 @@
+#include <pebble.h>
+
+#define MSG_KEY_DATA_SIZE 0
+
+static Window *window;
+static MenuLayer *s_menu_layer;
+
+static uint8_t friend_amt = 0;
+static bool loaded = false;
+static char** friend_names;
+static uint32_t* friend_steps;
+
+typedef enum {
+    TimePeriodMorning,
+    TimePeriodEvening,
+} TimePeriod;
+
+static TimePeriod time_period;
+
+static uint32_t your_steps = 0;
+
+static void send_user_steps() {
+    // This used to run only when launch_reason() == APP_LAUNCH_WAKEUP.
+    DictionaryIterator* iter;
+    app_message_outbox_begin(&iter);
+    dict_write_uint32(iter, 25973, your_steps);
+    app_message_outbox_send();
+}
+
+static uint16_t main_menu_row_amt(MenuLayer *menu_layer,
+        uint16_t section_num, void *callback_context) {
+    if (!loaded) {
+        return 2;
+    } else if (loaded && friend_amt == 0) {
+        return 2;
+    } else {
+        return 1 + friend_amt;
+    }
+}
+
+static int16_t main_menu_row_size(MenuLayer *menu_layer,
+        MenuIndex *cell_index, void *callback_context) {
+    if (cell_index->row == 0) {
+        return 50;
+    }
+    if (menu_layer_get_center_focused(menu_layer)) {
+        if (menu_layer_get_selected_index(menu_layer).row == cell_index->row) {
+            return 96;
+        } else {
+            return 42;
+        }
+    }
+    return 95;
+}
+
+static void main_menu_draw_row(GContext *ctx, const Layer *cell_layer,
+        MenuIndex *cell_index, void *callback_context) {
+    MenuLayer *menu_layer = (MenuLayer*) callback_context;
+    GRect bounds = layer_get_bounds(cell_layer);
+    if (cell_index->row == 0) {
+        GRect draw_rect = layer_get_bounds(cell_layer);
+        draw_rect.size.w -= 20;
+        draw_rect.origin.x += 10;
+        draw_rect.origin.y += 10;
+        char user_info[40];
+        snprintf(user_info, 40, "You took %lu steps this %s.",
+            your_steps,
+            time_period == TimePeriodMorning ? "morning" : "evening");
+        graphics_draw_text(ctx, user_info,
+            fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD),
+            draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+    } else if (cell_index->row == 1 && !loaded) {
+        GRect draw_rect = layer_get_bounds(cell_layer);
+        draw_rect.size.w -= 20;
+        draw_rect.origin.x += 10;
+        if (menu_layer_get_center_focused(menu_layer) &&
+            menu_layer_get_selected_index(menu_layer).row != cell_index->row) {
+            draw_rect.origin.y += 2;
+        } else {
+            draw_rect.origin.y += PBL_IF_ROUND_ELSE(26 + 16, 26 + 8);
+            graphics_draw_text(ctx, "Have you selected a username in the settings?",
+                fonts_get_system_font(FONT_KEY_GOTHIC_18),
+                draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+            draw_rect.origin.y -= PBL_IF_ROUND_ELSE(33, 30);
+            draw_rect.size.h -= 50;
+        }
+        graphics_draw_text(ctx, "Loading...",
+            fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD),
+            draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+    } else if (cell_index->row == 1 && friend_amt == 0) { // No friends yet
+        GRect draw_rect = layer_get_bounds(cell_layer);
+        draw_rect.size.w -= 20;
+        draw_rect.origin.x += 10;
+        draw_rect.origin.y += 8;
+        graphics_draw_text(ctx, "You can add friends in the app settings. "
+                                "They'll show up here about a day later.",
+            fonts_get_system_font(FONT_KEY_GOTHIC_18),
+            draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+    } else if (cell_index->row - 1 < friend_amt) {
+        uint8_t friend_num = cell_index->row - 1;
+        GRect draw_rect = layer_get_bounds(cell_layer);
+        if (menu_layer_get_center_focused(menu_layer) &&
+            menu_layer_get_selected_index(menu_layer).row != cell_index->row) {
+            graphics_draw_text(ctx, friend_names[friend_num],
+                fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD),
+                draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+            return;
+        } else {
+            graphics_draw_text(ctx, friend_names[friend_num],
+                fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD),
+                draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+        }
+        draw_rect.origin.y += 28;
+        char friendly_info_buff_1[10];
+        char friendly_info_buff_2[50];
+        uint8_t top_line_width = 0;
+        uint8_t second_text_width = 0;
+        snprintf(friendly_info_buff_1, 20,
+            "%lu",
+            friend_steps[friend_num]);
+        GTextAttributes *text_attrs = graphics_text_attributes_create();
+        graphics_text_attributes_enable_screen_text_flow(text_attrs, 10);
+        {
+            GSize text_size;
+            text_size = graphics_text_layout_get_content_size("took ",
+                fonts_get_system_font(FONT_KEY_GOTHIC_18), draw_rect,
+                GTextOverflowModeWordWrap, GTextAlignmentCenter);
+            top_line_width += text_size.w;
+            text_size = graphics_text_layout_get_content_size(friendly_info_buff_1,
+                fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), draw_rect,
+                GTextOverflowModeWordWrap, GTextAlignmentCenter);
+            top_line_width += text_size.w;
+            second_text_width = text_size.w;
+        }
+        snprintf(friendly_info_buff_2, 50,
+            "steps last %s.\nCan you beat them?",
+            time_period == TimePeriodMorning ? "morning" : "evening");
+
+        graphics_draw_text(ctx, "took ",
+            fonts_get_system_font(FONT_KEY_GOTHIC_18),
+            (GRect) {
+                .origin = GPoint((bounds.size.w - top_line_width) / 2,
+                                 draw_rect.origin.y),
+                .size = draw_rect.size
+            }, GTextOverflowModeWordWrap, GTextAlignmentLeft, text_attrs);
+        graphics_draw_text(ctx, friendly_info_buff_1,
+            fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD),
+            (GRect) {
+                .origin = GPoint((bounds.size.w + top_line_width - 2 * second_text_width) / 2,
+                                 draw_rect.origin.y),
+                .size = draw_rect.size
+            }, GTextOverflowModeWordWrap, GTextAlignmentLeft, text_attrs);
+
+        draw_rect.origin.y += 17;
+        graphics_draw_text(ctx, friendly_info_buff_2,
+            fonts_get_system_font(FONT_KEY_GOTHIC_18),
+            draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, text_attrs);
+        // draw_rect.origin.y += 24 + 10;
+
+        // This code was used to display a split screen view of steps - you/person
+
+        // graphics_draw_line(ctx, GPoint(bounds.size.w / 2, draw_rect.origin.y),
+        //                         GPoint(bounds.size.w / 2, draw_rect.origin.y + 40));
+        //
+        // const uint8_t text_offset = 5;
+
+        // draw_rect.size.w = bounds.size.w / 2 - text_offset;
+        // draw_rect.origin.x = text_offset;
+        // char buffer_they_steps[6];
+        // snprintf(buffer_they_steps, 5, "%lu", friend_steps[friend_num]);
+        // graphics_draw_text(ctx, buffer_they_steps,
+        //     fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS),
+        //     draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+        // draw_rect.origin.x = bounds.size.w / 2;
+        //
+        // char buffer_your_steps[6];
+        // snprintf(buffer_your_steps, 5, "%lu", your_steps);
+        // graphics_draw_text(ctx, buffer_your_steps,
+        //     fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS),
+        //     draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+        // draw_rect.origin.y += 22;
+        //
+        // draw_rect.size.w = bounds.size.w / 2 - text_offset;
+        // draw_rect.origin.x = text_offset;
+        // graphics_draw_text(ctx, "THEY",
+        //     fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD),
+        //     draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+        // draw_rect.origin.x = bounds.size.w / 2;
+        // graphics_draw_text(ctx, "YOU",
+        //     fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD),
+        //     draw_rect, GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+    }
+}
+
+static void window_load(Window *window) {
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+
+    s_menu_layer = menu_layer_create(bounds);
+    menu_layer_set_callbacks(s_menu_layer, s_menu_layer, (MenuLayerCallbacks) {
+        .get_num_rows = main_menu_row_amt,
+        .draw_row = main_menu_draw_row,
+        .get_cell_height = main_menu_row_size,
+    });
+    menu_layer_set_click_config_onto_window(s_menu_layer, window);
+    menu_layer_set_normal_colors(s_menu_layer, GColorWhite, GColorBlack);
+    menu_layer_set_highlight_colors(s_menu_layer, GColorVividCerulean, GColorWhite);
+    layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+}
+
+static void window_unload(Window *window) {
+}
+
+static void inbox_handler(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Inbox handler");
+    Tuple *friend_amt_tup = dict_find(iterator, MSG_KEY_DATA_SIZE);
+    friend_amt = friend_amt_tup->value->uint8;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%u friends sent.", friend_amt);
+    friend_names = malloc(sizeof(char*) * friend_amt);
+    friend_steps = malloc(sizeof(uint32_t) * friend_amt);
+    for (uint8_t i = 0; i < friend_amt; i++) {
+        Tuple *this_friend_name_tup = dict_find(iterator, 100 + i);
+        friend_names[i] = malloc(this_friend_name_tup->length);
+        strcpy(friend_names[i], this_friend_name_tup->value->cstring);
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Friend name: %s", friend_names[i]);
+
+        Tuple *this_friend_data_tup = dict_find(iterator, 200 + i);
+        friend_steps[i] = this_friend_data_tup->value->uint32;
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Friend steps: %lu", friend_steps[i]);
+    }
+    loaded = true;
+    menu_layer_reload_data(s_menu_layer);
+
+    send_user_steps();
+}
+
+static time_t today_beginning() {
+    time_t now = time(NULL);
+    struct tm here_time = *localtime(&now);
+    here_time.tm_hour = 0;
+    here_time.tm_min = 0;
+    here_time.tm_sec = 0;
+    return mktime(&here_time);
+}
+
+static void init(void) {
+    window = window_create();
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload,
+    });
+    const bool animated = true;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Pushing");
+    window_stack_push(window, animated);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Pushed");
+    HealthServiceAccessibilityMask availability = health_service_metric_accessible(HealthMetricStepCount,
+                                         time(NULL) - 360,
+                                         time(NULL));
+    if (availability == HealthServiceAccessibilityMaskAvailable) {
+        if (clock_to_timestamp(TODAY, 12, 0) > time(NULL)) { // At or before 11:59am
+            your_steps = health_service_sum(HealthMetricStepCount,
+                today_beginning(),
+                time(NULL));
+            time_period = TimePeriodMorning;
+        } else {
+            your_steps = health_service_sum(HealthMetricStepCount,
+                today_beginning() + 12*60*60,
+                time(NULL));
+            time_period = TimePeriodEvening;
+        }
+        // your_steps = health_service_sum_today(HealthMetricStepCount);
+    }
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "available: %u", availability == HealthServiceAccessibilityMaskAvailable);
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "nopermission: %u", availability & HealthServiceAccessibilityMaskNoPermission);
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "notsupported: %u", availability & HealthServiceAccessibilityMaskNotSupported);
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "notavailable: %u", availability & HealthServiceAccessibilityMaskNotAvailable);
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "availability: %u", availability);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Steps gotten");
+
+    app_message_register_inbox_received(inbox_handler);
+    app_message_open(1024, 128);
+}
+
+static void deinit(void) {
+    window_destroy(window);
+    WeekDay week[7] = {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY};
+    time_t closestFutureTime = time(NULL) + 10*24*60*60;
+    bool isNoonWakeup = false;
+    for (uint8_t i = 0; i < 7; i++) {
+        time_t temp1 = clock_to_timestamp(week[i], 0, 0);
+        time_t temp2 = clock_to_timestamp(week[i], 12, 0);
+        if (temp1 < closestFutureTime) {
+            APP_LOG(APP_LOG_LEVEL_DEBUG, "%lu is smaller", temp1);
+            closestFutureTime = temp1;
+            isNoonWakeup = false;
+        }
+        if (temp2 < closestFutureTime) {
+            APP_LOG(APP_LOG_LEVEL_DEBUG, "%lu is smaller [2]", temp2);
+            closestFutureTime = temp2;
+            isNoonWakeup = true;
+        }
+    }
+    wakeup_cancel_all();
+    wakeup_schedule(closestFutureTime, isNoonWakeup + 40, false);
+    closestFutureTime += 12*60*60;
+    wakeup_schedule(closestFutureTime, (!isNoonWakeup) + 40, false);
+    closestFutureTime += 12*60*60;
+    wakeup_schedule(closestFutureTime, isNoonWakeup + 40, false);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%lu and %u", closestFutureTime, isNoonWakeup);
+}
+
+int main(void) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Initing");
+    init();
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+    app_event_loop();
+    deinit();
+}
---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,154 @@
+/*jshint -W069*/
+
+var tltoken = '';
+
+function get_day_half() {
+    return (new Date().getHours() > 12) ? 'PM' : 'AM';
+}
+
+function gen_time_string(hoursinfuture) {
+    var now = new Date();
+    now.setHours(now.getHours() + hoursinfuture);
+    var time = now.toISOString().substring(0, 10);
+    time += '-TP-';
+    time += (now.getHours() > 13 ||
+             now.getHours() < 1) ? 'PM' : 'AM';
+    return time;
+}
+
+function send_tl_token(token) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == this.DONE) {
+            console.log(this.responseText);
+        }
+    };
+    xhr.open('GET', 'https://pythonbackend-stepupforpebble.rhcloud.com/' +
+                    'set_timeline_token?uid=' + Pebble.getAccountToken() +
+                    '&tltoken=' + token);
+    xhr.send();
+    // It's possible that this fails.
+    // Failing is ignored for a reason:
+    //     If this fails, that means that
+    //     the user doesn't exist.
+    //     On the next launch, the app will
+    //     retry sending the timeline token.
+}
+
+function send_pin(data) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == this.DONE) {
+            console.log('Pin push response: ' + this.responseText);
+            console.log('Pin push status code: ' + this.status);
+        }
+    };
+    xhr.open('PUT', 'https://timeline-api.getpebble.com/v1/user/pins/' + data.id);
+    xhr.setRequestHeader('X-User-Token', tltoken);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+    console.log(JSON.stringify(data));
+}
+
+function send_twice_daily_pin() {
+    var timestr = gen_time_string(12);
+    console.log(timestr);
+    var hourofday = timestr.slice(-2) == "AM" ? 6 : 12;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == this.DONE) {
+            var json = JSON.parse(this.responseText);
+            var challenge = json[0];
+            var pin = {
+                'id': 'stepup-pin-' + timestr,
+                'time': new Date(new Date(timestr.slice(0, 10)).setHours(hourofday)).toISOString(),
+                'layout': {
+                    'type': 'genericPin',
+                    'title': 'Health Challenge!',
+                    'subtitle': 'Beat: ' + challenge.username,
+                    'body': challenge.username + ' reached ' + challenge.steps +
+                            ' steps last ' +
+                            (timestr.slice(-2) == 'AM' ? 'morning' : 'afternoon') +
+                            '. Can you one-up them?',
+                    'tinyIcon': 'system://images/STOCKS_EVENT'
+                }
+            };
+            console.log(JSON.stringify(challenge));
+            console.log(JSON.stringify(pin));
+            send_pin(pin);
+        } else {
+            console.error(this.responseText);
+        }
+    };
+    xhr.open('GET', 'https://pythonbackend-stepupforpebble.rhcloud.com/' +
+                    'get_active_friends?uid=' + Pebble.getAccountToken() +
+                    '&dayhalf=' + timestr.slice(-2));
+    xhr.send();
+}
+
+function send_hist_data(data) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == this.DONE) {
+            console.log(this.responseText);
+        }
+    };
+    var time = gen_time_string(0);
+    xhr.open('GET', 'https://pythonbackend-stepupforpebble.rhcloud.com/' +
+                    'add_data_point?uid=' + Pebble.getAccountToken() +
+                    '&steps=' + data + '&timeperiod=' + time);
+    xhr.send();
+}
+
+function fetch_other_user_data() {
+    var xhr = new XMLHttpRequest();
+    var dayhalf = get_day_half();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == this.DONE) {
+            var data = {'MSG_KEY_DATA_SIZE': 0};
+            var json = JSON.parse(this.responseText);
+            if (json.length > 0) {
+                data['MSG_KEY_DATA_SIZE'] = Math.min(5, json.length);
+                for (var i = 0; i < Math.min(5, json.length); i++) {
+                    data[100 + i] = json[i].username;
+                    data[200 + i] = json[i].steps;
+                }
+            }
+            console.log(JSON.stringify(data));
+            Pebble.sendAppMessage(data);
+        } else {
+            console.error(this.responseText);
+        }
+    };
+    xhr.open('GET', 'https://pythonbackend-stepupforpebble.rhcloud.com/' +
+                    'get_active_friends?uid=' + Pebble.getAccountToken() +
+                    '&dayhalf=' + dayhalf);
+    xhr.send();
+}
+
+Pebble.addEventListener('appmessage', function(data) {
+    console.log('AppMessage received');
+    console.log(data.payload[25973]);
+    send_hist_data(data.payload[25973]);
+});
+
+Pebble.addEventListener('showConfiguration', function() {
+    var url = 'https://step-up-health.github.io/step-up-config-page/#' +
+        Pebble.getAccountToken() + ':' + tltoken;
+    console.log('Showing configuration page: ' + url);
+    Pebble.openURL(url);
+});
+
+Pebble.addEventListener('ready', function() {
+    send_twice_daily_pin();
+    var uid = Pebble.getAccountToken();
+    console.log('Hello');
+    localStorage.tlTokenSent = localStorage.tlTokenSent || false;
+    Pebble.getTimelineToken(function(token) {
+        tltoken = token;
+        send_tl_token(token);
+    }, function(error) {
+        console.error(error);
+    });
+    fetch_other_user_data();
+});
